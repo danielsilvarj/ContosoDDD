45ce5acb3f92e9a8eaa429a8291e1468fafaa84c
danielsilvarj <daniel.cg.rio@gmail.com>
2020-06-19 10:31:19 -0300

    Camada de Persistencia: Configurações

---
+++ ContosoDDD.Infraestrutura.Persistencia/Configuracoes/AlunoConfiguracao.cs
@@ -0,0 +1,26 @@
+﻿using ContosoDDD.Dominio.Entidade;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace ContosoDDD.Infraestrutura.Persistencia.Configuracoes
+{
+    public class AlunoConfiguracao : IEntityTypeConfiguration<Aluno>
+    {
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.ToTable("Alunos");
+            builder.Property(a => a.DataMatricula)
+                .HasColumnName("Matricula")
+                .IsRequired()
+                .HasColumnType("Date")
+                .HasDefaultValueSql("GetDate()");
+            builder.Property(a => a.DataCadastro)
+                .HasColumnName("Cadastro")
+                .IsRequired()
+                .HasColumnType("Date")
+                .HasDefaultValueSql("GetDate()");
+        }
+    }
+}
--- ContosoDDD.Infraestrutura.Persistencia/ContosoDbContext.cs
+++ ContosoDDD.Infraestrutura.Persistencia/ContosoDbContext.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Text;
+﻿using ContosoDDD.Dominio.Entidade;
+using ContosoDDD.Infraestrutura.Persistencia.Configuracoes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace ContosoDDD.Infraestrutura.Persistencia
 {
-    public class ContosoDbContext: DbContext
+    public class ContosoDbContext : DbContext
     {
-        
+        public ContosoDbContext(DbContextOptions<ContosoDbContext> options)
+            :base(options)
+        {
+
+        }
+
+
+        public DbSet<Aluno> Alunos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ContosoDDD;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach(var prop in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(string)))
+            {
+                if (prop.GetColumnType() == null)
+                    prop.SetColumnType("varchar(255)");
+            }
+
+            modelBuilder.ApplyConfiguration(new AlunoConfiguracao());
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
     }
 }
