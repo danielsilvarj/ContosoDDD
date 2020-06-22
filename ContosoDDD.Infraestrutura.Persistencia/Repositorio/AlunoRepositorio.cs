4f45fc4469c5c09a1ae77e5eab5a8944fa29f97d
danielsilvarj <daniel.cg.rio@gmail.com>
2020-06-21 21:32:38 -0300

    AlunoRepositorio

---
+++ ContosoDDD.Infraestrutura.Persistencia/Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using ContosoDDD.Dominio.Entidade;
+using ContosoDDD.Dominio.Interfaces.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContosoDDD.Infraestrutura.Persistencia.Repositorio
+{
+    public class AlunoRepositorio : BaseRepositorio<Aluno>, IAlunoRepositorio
+    {
+
+        public readonly ContosoDbContext _contexto;
+
+        public AlunoRepositorio(ContosoDbContext contexto) : base (contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public Aluno DetalharPorNome(string busca)
+        {
+            return _contexto.Alunos.FirstOrDefault(a => (a.Nome + " " + a.Sobrenome).Contains(busca));
+        }
+
+        public IEnumerable<Aluno> ListarAlunosAtivos()
+        {
+            return _contexto.Alunos.Where(a => a.AlunoAtivo()).ToList();
+        }
+    }
+}
